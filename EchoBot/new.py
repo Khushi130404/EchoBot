4d94d2f07d2daf62dc6c0b0b4c17185024fdbb1d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-12-12 11:39:37 +0530

    Sorting Word & Classes

--- EchoBot/new.py
+++ EchoBot/new.py
@@ -13,3 +13,18 @@ words = []
 documemts = []
 classes = []
 ignores = ['!',',','.','?','/']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = nltk.word_tokenize(pattern)
+        documemts.append(wordList,intent['tag'])
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+words = [lemmatizer.lemmatize(words) for word in words if word not in ignores]
+words = sorted(set(classes))
+classes = sorted(set(classes))
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
