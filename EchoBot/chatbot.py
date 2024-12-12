e3e19865c2bc5bd15c44f6888006b32088a47c5c
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-12-12 13:20:35 +0530

    Cleanup Sentence

--- EchoBot/chatbot.py
+++ EchoBot/chatbot.py
@@ -14,3 +14,8 @@ intents = json.loads(open('../intents.json').read())
 words = pickle.loads(open('words.pkl','rb'))
 classes = pickle.loads(open('classes.pkl','rb'))
 model = load_model('echobot_model.h5')
+
+def cleanup_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
